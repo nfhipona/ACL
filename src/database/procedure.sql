8b5bde0d722454bd5dbbbc6a97b1ac8531bddc23
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-09-27 00:19:06 +0800

    Add store procedure to validate permission

--- src/database/procedure.sql
+++ src/database/procedure.sql
@@ -6,7 +6,10 @@ DELIMITER $$
 
 -- BINARY to UUID
 DROP FUNCTION IF EXISTS BIN_TO_UUID;
-CREATE FUNCTION BIN_TO_UUID(b BINARY(16), f BOOLEAN)
+CREATE FUNCTION BIN_TO_UUID(
+    b BINARY(16), 
+    f BOOLEAN
+)
 RETURNS CHAR(36)
 DETERMINISTIC
 BEGIN
@@ -27,7 +30,10 @@ END $$
 
 -- UUID to BINARY
 DROP FUNCTION IF EXISTS UUID_TO_BIN;
-CREATE FUNCTION UUID_TO_BIN(uuid CHAR(36), f BOOLEAN)
+CREATE FUNCTION UUID_TO_BIN(
+    uuid CHAR(36), 
+    f BOOLEAN
+)
 RETURNS BINARY(16)
 DETERMINISTIC
 BEGIN
@@ -42,6 +48,37 @@ BEGIN
     );
 
     RETURN UNHEX(hexStr);
+END $$
+
+--
+-- ACL Validation Settings
+--
+DROP PROCEDURE IF EXISTS ACL_VALIDATE;
+CREATE PROCEDURE ACL_VALIDATE(
+    resource_code VARCHAR(255), 
+    role_id CHAR(36), 
+    mode CHAR(5), 
+    f BOOLEAN
+)
+DETERMINISTIC
+BEGIN
+    SET @code = resource_code;
+    SET @uuid = UUID_TO_BIN(role_id, f);
+    SET @regExp = CONCAT('[', mode, ']');
+    SET @query = CONCAT(
+        'SELECT COUNT(p.role_id) FROM acl_permission p ',
+        'INNER JOIN acl_role r ON r.id = p.role_id ',
+        'INNER JOIN acl_resource s ON s.id = p.resource_id '
+        'INNER JOIN acl_access_type a ON a.id = p.access_type_id ',
+        'WHERE s.deleted <> 1 AND p.is_disabled <> 1 AND s.code = ? AND r.id = ? AND a.code REGEXP ? ',
+        'INTO @hasPermission'
+    );
+
+    PREPARE stmt FROM @query;
+    EXECUTE stmt USING @code, @uuid, @regExp;
+    DEALLOCATE PREPARE stmt;
+    
+    SELECT @hasPermission;
 END $$
 
 DELIMITER ;
